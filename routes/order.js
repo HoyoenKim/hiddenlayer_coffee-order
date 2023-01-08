323e85353e10c892b101fad79ddb9548b63d9cdf
kimhoyeon <hoyeon@ip-172-31-34-160.ap-northeast-2.compute.internal>
2023-01-08 17:31:07 +0000

    order

--- routes/order.js
+++ routes/order.js
@@ -7,8 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  console.log(req.body);
-  
+  console.log(req.body.name);  
   const responseBody = {
     version: "2.0",
     template: {
